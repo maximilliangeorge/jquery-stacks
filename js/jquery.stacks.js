dbe73464450e5895c221df6bf5a6c7c32f768268
Maximillian George <maximillian.george@gmail.com>
2015-10-29 21:52:15 +0100

    Initial commit

---
+++ js/jquery.stacks.js
@@ -0,0 +1,38 @@
+(function ( $ ) {
+  $.fn.stack = function() {
+      this.css( "position", "fixed" );
+      var elemCount = 0;
+      var elems = [];
+      var currentElem = 0;
+      var prevHeight = 0;
+      this.each(function() {
+        $('body').css('height', $('body').outerHeight() + $(this).outerHeight());
+        $(this).css('z-index', elemCount);
+        elems.push($(this));
+        console.log(elemCount);
+        elemCount--;
+      });
+
+      $(window).scroll(function() {
+        elems[currentElem].css('top', - $(window).scrollTop() + prevHeight);
+        checkElems();
+      });
+
+      function checkElems() {
+        if ($(window).scrollTop() > elems[currentElem].outerHeight() + prevHeight) {
+          prevHeight = prevHeight + elems[currentElem].outerHeight();
+          currentElem++;
+        }
+
+        if ($(window).scrollTop() < prevHeight) {
+          prevHeight = prevHeight - elems[currentElem].outerHeight();
+          currentElem--;
+        }
+      }
+
+  };
+}( jQuery ));
+
+$(document).ready(function(){
+  $('div').stack();
+});
